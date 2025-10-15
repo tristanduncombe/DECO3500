25670e7fe2a14df0128a5fee0930042578d5dbe4
PramithKodali <pramithkodali14@gmail.com>
2025-10-15 22:30:58 +1000

    config update

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,9 +1,10 @@
-module.exports = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
-}
+};
+
+export default nextConfig;
